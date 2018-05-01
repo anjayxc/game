b8619885a6d384e770788c0f8dc89c3fe431be10
xieshuhan <330136757@qq.com>
2018-05-01 23:07:54 +0800

    Finish RomanToDecimal.

--- RomanNumeral/src/test/java/com/soda/roman/RomanToDecimalTest.java
+++ RomanNumeral/src/test/java/com/soda/roman/RomanToDecimalTest.java
@@ -28,9 +28,39 @@ public class RomanToDecimalTest {
     public void convert_II() {
         assertEquals(2, convert("II"));
     }
+
     @Test
     public void convert_IV() {
         assertEquals(4, convert("IV"));
+    }
+
+    @Test
+    public void convert_XC() {
+        assertEquals(90, convert("XC"));
+    }
+
+    @Test
+    public void convert_CM() {
+        assertEquals(900, convert("CM"));
+    }
+
+    @Test
+    public void convert_VC() {
+        assertEquals(105, convert("VC"));
+    }
+
+    @Test
+    public void convert_DM() {
+        assertEquals(1500, convert("DM"));
+    }
+
+    @Test
+    public void convert_MMVI() {
+        assertEquals(2006, convert("MMVI"));
+    }
+    @Test
+    public void convert_MCMXLIV() {
+        assertEquals(1944, convert("MCMXLIV"));
     }
 
     private int convert(String romanNumeral) {
