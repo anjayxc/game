e73dcc8b2c0bf072adf42b8f68d36321a0b00677
xieshuhan <330136757@qq.com>
2018-05-01 23:03:15 +0800

    test case

--- RomanNumeral/src/main/java/com/soda/roman/Roman.java
+++ RomanNumeral/src/main/java/com/soda/roman/Roman.java
@@ -1,7 +1,40 @@
 package com.soda.roman;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Roman {
-    public int convertRomanToDecimal(String romanNumberal) {
-        return 1;
+
+    private Map<String, Integer> dict;
+
+    {
+        dict = new HashMap<>();
+        dict.put("M", 1000);
+        dict.put("CM", 900);
+        dict.put("D", 500);
+        dict.put("CD", 400);
+        dict.put("C", 100);
+        dict.put("XC", 90);
+        dict.put("L", 50);
+        dict.put("XL", 40);
+        dict.put("X", 10);
+        dict.put("IX", 9);
+        dict.put("V", 5);
+        dict.put("IV", 4);
+        dict.put("I", 1);
+    }
+
+    public int convertRomanToDecimal(String romanNumeral) {
+        int result = 0;
+        for (int i = 0; i < romanNumeral.length(); i++) {
+            if (i + 1 < romanNumeral.length() && dict.get(romanNumeral.substring(i, i + 1) + romanNumeral.substring(i + 1, i + 2)) != null) {
+                result += dict.get(romanNumeral.substring(i, i + 1) + romanNumeral.substring(i + 1, i + 2));
+                i++;
+            } else {
+                result += dict.get(romanNumeral.substring(i, i + 1));
+            }
+        }
+
+        return result;
     }
 }
--- RomanNumeral/src/test/java/com/soda/roman/RomanToDecimalTest.java
+++ RomanNumeral/src/test/java/com/soda/roman/RomanToDecimalTest.java
@@ -1,14 +1,39 @@
 package com.soda.roman;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class RomanToDecimalTest {
 
+    Roman roman = null;
+
+    @Before
+    public void setUp() {
+        roman = new Roman();
+    }
+
     @Test
     public void convert_I() {
-        Roman roman = new Roman();
-        assertEquals(1, roman.convertRomanToDecimal("I"));
+        assertEquals(1, convert("I"));
+    }
+
+    @Test
+    public void convert_V() {
+        assertEquals(5, convert("V"));
+    }
+
+    @Test
+    public void convert_II() {
+        assertEquals(2, convert("II"));
+    }
+    @Test
+    public void convert_IV() {
+        assertEquals(4, convert("IV"));
+    }
+
+    private int convert(String romanNumeral) {
+        return roman.convertRomanToDecimal(romanNumeral);
     }
 }
