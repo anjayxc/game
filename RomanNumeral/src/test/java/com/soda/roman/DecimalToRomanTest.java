916d4ab5944345198c5522fbbe2b3400a41e5994
xieshuhan <330136757@qq.com>
2018-05-01 23:15:35 +0800

    Test case.

--- RomanNumeral/src/test/java/com/soda/roman/DecimalToRomanTest.java
+++ RomanNumeral/src/test/java/com/soda/roman/DecimalToRomanTest.java
@@ -17,13 +17,26 @@ public class DecimalToRomanTest {
 
     @Test
     public void convert_1() {
-        assertEquals("I", roman.convertDecimalToRoman(1));
+        assertEquals("I", convert(1));
     }
 
     @Test
     public void convert_5() {
-        assertEquals("V", roman.convertDecimalToRoman(5));
+        assertEquals("V", convert(5));
     }
 
+    @Test
+    public void convert_500() {
+        assertEquals("D", convert(500));
+    }
+
+    @Test
+    public void convert_900() {
+        assertEquals("CM", convert(900));
+    }
+
+    private String convert(int decimalNumeral) {
+        return roman.convertDecimalToRoman(decimalNumeral);
+    }
 
 }
